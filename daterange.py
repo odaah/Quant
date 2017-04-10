d0541d59ac848b081825e32c630f1f0b29853e1f
odaah <vijay.emarose@gmail.com>
2017-04-09 21:59:59 -0500

    commit

--- daterange.py
+++ daterange.py
@@ -5,12 +5,16 @@ def test_run():
 	end_date='2017-01-26'
 	dates=pd.date_range(start_date,end_date)
 
+	#Sets the date as the index
 	df1=pd.DataFrame(index=dates)
 
+	#Reads csv file with Date as the index and uses 3 columns, ignoring NaN
 	dfSPY=pd.read_csv("data/SPY.csv",index_col='Date',parse_dates=True,usecols=['Date','Adj Close', 'Close'],na_values=['NaN'])
 
+	#Joins only the dates provided in the date range
 	df1=df1.join(dfSPY)
 	
+	#Drops NaN
 	df1=df1.dropna()
 	print df1
 
